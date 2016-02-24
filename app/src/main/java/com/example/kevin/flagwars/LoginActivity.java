ce058fba42860467abd4e551943606c62627dfa0
L3monz <brianlemons2013@gmail.com>
2016-02-24 18:27:21 -0500

    Brian changes.

--- app/src/main/java/com/example/kevin/flagwars/LoginActivity.java
+++ app/src/main/java/com/example/kevin/flagwars/LoginActivity.java
@@ -43,6 +43,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
+    private int Brian = 69;
 
     /**
      * A dummy authentication store containing known user names and passwords.
@@ -65,7 +66,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println(s);
         setContentView(R.layout.activity_login);
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
