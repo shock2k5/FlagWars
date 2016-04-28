0d15e7903f15f9a364c1f251ad5720b14fd6cc03
Aditya Ayyakad <njaditya@gmail.com>
2016-04-27 20:04:30 -0400

    Finished Game
    Game

--- app/src/main/java/com/example/kevin/flagwars/Game.java
+++ app/src/main/java/com/example/kevin/flagwars/Game.java
@@ -1,30 +1,89 @@
 package com.example.kevin.flagwars;
 
+import com.parse.ParseException;
 import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Adi on 4/27/16.
  */
+
 public class Game {
     /**
      *  public is true private is false
      */
     boolean visibility;
-    String name;
+    String name, code;
     ParseGeoPoint location;
     int numPlayers;
     int timeLimit;
     ArrayList<String> playerNames;
     ParseGeoPoint[] flagLocations;
 
-    public Game(String name, boolean visibility, ParseGeoPoint location,
-                int numPlayers, int timeLimit, ArrayList<String> playerNames, ParseGeoPoint[] flagLocations) {
+    public Game(String name, String code, boolean visibility, ParseGeoPoint location,
+                int numPlayers, int timeLimit, ArrayList<String> playerNames,
+                ParseGeoPoint[] flagLocations) {
         this.name = name;
+        this.code = code;
         this.visibility = visibility;
         this.location = location;
         this.numPlayers = numPlayers;
         this.timeLimit = timeLimit;
         this.playerNames = playerNames;
+        this.flagLocations = flagLocations;
+    }
+
+    public Game(String name, String code, boolean visibility, ParseGeoPoint location,
+                int numPlayers, int timeLimit, Object playerNames, Object flagLocations) {
+        this.name = name;
+        this.code = code;
+        this.visibility = visibility;
+        this.location = location;
+        this.numPlayers = numPlayers;
+        this.timeLimit = timeLimit;
+        this.playerNames = (ArrayList<String>) playerNames;
+        this.flagLocations = (ParseGeoPoint[]) flagLocations;
+    }
+
+    public static List<String> getGameNames(List<Game> gameList) {
+        List<String> names = new ArrayList<String>();
+
+        for (Game g : gameList)
+            names.add(g.name);
+
+        return names;
+    }
+
+    public static ParseObject convertGameToParseObject(Game g) {
+        ParseObject o = ParseObject.create("Game");
+        o.add("name", g.name);
+        o.add("code", g.code);
+        o.add("visibility", g.visibility);
+        o.add("location", g.location);
+        o.add("numPlayers", g.numPlayers);
+        o.add("timeLimit", g.timeLimit);
+        o.add("playerNames", g.playerNames);
+        return o;
+    }
+
+    public static Game getObjectFromParse(String objectId) {
+        ParseQuery q = ParseQuery.getQuery("Game");
+        Game g = null;
+        try {
+            g = objectRetrievalSuccessful(q.get(objectId));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return g;
+    }
+
+    private static Game objectRetrievalSuccessful(ParseObject o) {
+        return new Game(o.getString("name"), o.getString("code"), o.getBoolean("visibility"),
+                o.getParseGeoPoint("location"), o.getInt("numPlayers"), o.getInt("timeLimit"),
+                o.get("playerNames"), o.get("flagLocations"));
     }
 }
--- app/src/main/java/com/example/kevin/flagwars/JoinGameActivity.java
+++ app/src/main/java/com/example/kevin/flagwars/JoinGameActivity.java
@@ -1,24 +1,67 @@
 package com.example.kevin.flagwars;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.Currency;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class JoinGameActivity extends AppCompatActivity {
 
     ListView mGameListView;
     TextView mEnterCodeTextView;
+    List<Game> gameList = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_game);
 
         mGameListView = (ListView) findViewById(R.id.gameListView);
-        mEnterCodeTextView = (TextView) findViewById(R.id.enterCodeEditText);
+        mEnterCodeTextView = (TextView) findViewById(R.id.enterCodeTextView);
 
+        ParseGeoPoint currentLocation = ParseGeoPoint.getCurrentLocationInBackground(100).getResult();
+        ParseQuery q = ParseQuery.getQuery("Game");
+        q.whereWithinMiles("location", currentLocation, 2.0);
+        q.setLimit(10);
+        System.out.println(currentLocation.toString() + "\nAbout to be done");
+        q.findInBackground(new FindCallback() {
+            @Override
+            public void done(List objects, ParseException e) {
+                if (e == null) {
+                    System.out.println("Success");
+                    objectRetrievalSucceeded(objects);
+                } else { // error
+                    System.out.println(e.getLocalizedMessage());
+                }
+            }
 
+            @Override
+            public void done(Object o, Throwable t) {
+                if (t == null) {
+                    System.out.println(o);
+                } else {
+                    System.out.println(t.getLocalizedMessage());
+                }
+            }
+        });
+    }
+
+    private void objectRetrievalSucceeded(List objects) {
+        gameList = objects;
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Game.getGameNames(gameList));
+        this.mGameListView.setAdapter(arrayAdapter);
+        System.out.println("Ended");
     }
 }
