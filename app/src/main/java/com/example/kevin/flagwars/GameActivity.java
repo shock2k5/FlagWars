aba00266ba390594a359d9077714c2f4cccbb8b2
Aditya Ayyakad <njaditya@gmail.com>
2016-05-17 04:59:28 -0400

    functional - minus end screen

--- app/src/main/java/com/example/kevin/flagwars/GameActivity.java
+++ app/src/main/java/com/example/kevin/flagwars/GameActivity.java
@@ -12,6 +12,8 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.firebase.client.DataSnapshot;
@@ -38,6 +40,9 @@ import java.util.Observer;
 public class GameActivity
         extends FragmentActivity
         implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    final float ZOOM_LEVEL = 16.5f;
+    final int RADIUS = 10;
+    final int REFRESH_INTERVAL = 100;
 
     private GoogleMap mMap;
     private Location loc = null;
@@ -46,17 +51,17 @@ public class GameActivity
     private GoogleApiClient myClient;
     private Firebase ref;
     private LocationListener locationListener;
-    final float ZOOM_LEVEL = 16.5f;
     private boolean reload = true;
     private FloatingActionButton mCaptureButton;
+    private float[] distance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        distance = new float[1];
         mCaptureButton = (FloatingActionButton) findViewById(R.id.capture_button);
 
-        Log.d("Debug", "Activity started");
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -64,8 +69,59 @@ public class GameActivity
         Firebase.setAndroidContext(this.getApplicationContext());
         ref = ImportantMethods.getFireBase().child("Game").child(getIntent().getStringExtra("gameUid"));
 
+
         currentUser = this.getIntent().getStringExtra("currentUser");
         teamColor = this.getIntent().getStringExtra("teamColor");
+
+        mCaptureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ref.child("holding").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        boolean bfLatBool = snapshot.child("blueFlagLatitude").getValue(Double.class) != null;
+                        boolean bfLongBool = snapshot.child("blueFlagLongitude").getValue(Double.class) != null;
+                        boolean rfLatBool = snapshot.child("redFlagLatitude").getValue(Double.class) != null;
+                        boolean rfLongBool = snapshot.child("redFlagLongitude").getValue(Double.class) != null;
+
+                        if (teamColor.equals("blue")) {
+                            if (bfLatBool && bfLongBool) {
+                                // red returnFlag
+                                ref.child("holding").child("redFlagLatitude").removeValue();
+                                ref.child("holding").child("redFlagLongitude").removeValue();
+                                Toast.makeText(GameActivity.this, "Red flag returned.", Toast.LENGTH_LONG).show();
+                            }
+
+                            if (rfLatBool && rfLongBool) {
+                                // blue grabFlag
+                                ref.child("holding").child("redFlagLatitude").setValue(loc.getLatitude());
+                                ref.child("holding").child("redFlagLongitude").setValue(loc.getLongitude());
+                                Toast.makeText(GameActivity.this, "Red flag captured.", Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            if (rfLatBool && rfLongBool) {
+                                // red returnFlag
+                                ref.child("holding").child("blueFlagLatitude").removeValue();
+                                ref.child("holding").child("blueFlagLongitude").removeValue();
+                                Toast.makeText(GameActivity.this, "Blue flag returned.", Toast.LENGTH_LONG).show();
+                            }
+
+                            if (bfLatBool && bfLongBool) {
+                                // red grabFlag
+                                ref.child("holding").child("blueFlagLatitude").setValue(loc.getLatitude());
+                                ref.child("holding").child("blueFlagLongitude").setValue(loc.getLongitude());
+                                Toast.makeText(GameActivity.this, "Blue flag captured.", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {
+                        Log.d("Debug", "GameActivity mCaptureButton onClickListener", firebaseError.toException());
+                    }
+                });
+            }
+        });
 
         myClient = new GoogleApiClient.Builder(GameActivity.this).addApi(LocationServices.API).addConnectionCallbacks(GameActivity.this)
                 .addOnConnectionFailedListener(GameActivity.this).build();
@@ -73,7 +129,6 @@ public class GameActivity
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(final Location location) {
-                Log.d("Debug", "Location found");
                 loc = location;
                 if (reload) {
                     mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(locationToLatLng(loc), ZOOM_LEVEL), 4000, null);
@@ -91,7 +146,6 @@ public class GameActivity
                         update.put("teamColor", teamColor);
 
                         ref.child("liveLocations").child(currentUser).updateChildren(update);
-                        Log.d("Debug", "Sent to Firebase");
                         return loc;
                     }
                 };
@@ -113,7 +167,7 @@ public class GameActivity
     public void onConnected(Bundle connectionHint) {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
-        LocationServices.FusedLocationApi.requestLocationUpdates(myClient, new LocationRequest().setInterval(500), locationListener);
+        LocationServices.FusedLocationApi.requestLocationUpdates(myClient, new LocationRequest().setInterval(REFRESH_INTERVAL), locationListener);
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
@@ -130,41 +184,72 @@ public class GameActivity
                         snapshot.child("blueFlagLongitude").getValue(Double.class) != null) {
                     HashMap<String, HashMap<String, Object>> liveLocationsMap =
                             (HashMap<String, HashMap<String, Object>>) snapshot.child("liveLocations").getValue();
+
+                    Double rfLat = snapshot.child("redFlagLatitude").getValue(Double.class);
+                    Double rfLong = snapshot.child("redFlagLongitude").getValue(Double.class);
+                    Double bfLat = snapshot.child("blueFlagLatitude").getValue(Double.class);
+                    Double bfLong = snapshot.child("blueFlagLongitude").getValue(Double.class);
+                    boolean holding = false;
+
+                    if (snapshot.child("holding").child("redFlagLatitude").getValue(Double.class) != null &&
+                            snapshot.child("holding").child("redFlagLongitude").getValue(Double.class) != null) {
+                        rfLat = snapshot.child("holding").child("redFlagLatitude").getValue(Double.class);
+                        rfLong = snapshot.child("holding").child("redFlagLongitude").getValue(Double.class);
+                        holding = true;
+                    }
+
+                    if (snapshot.child("holding").child("blueFlagLatitude").getValue(Double.class) != null &&
+                            snapshot.child("holding").child("blueFlagLongitude").getValue(Double.class) != null) {
+                        bfLat = snapshot.child("holding").child("blueFlagLatitude").getValue(Double.class);
+                        bfLong = snapshot.child("holding").child("blueFlagLongitude").getValue(Double.class);
+                        holding = true;
+                    }
+
                     game.redFlag = new Location(LocationManager.GPS_PROVIDER);
-                    game.redFlag.setLatitude(snapshot.child("redFlagLatitude").getValue(Double.class));
-                    game.redFlag.setLongitude(snapshot.child("redFlagLongitude").getValue(Double.class));
+                    game.redFlag.setLatitude(rfLat);
+                    game.redFlag.setLongitude(rfLong);
 
                     game.blueFlag = new Location(LocationManager.GPS_PROVIDER);
-                    game.blueFlag.setLatitude(snapshot.child("blueFlagLatitude").getValue(Double.class));
-                    game.blueFlag.setLongitude(snapshot.child("blueFlagLongitude").getValue(Double.class));
+                    game.blueFlag.setLatitude(bfLat);
+                    game.blueFlag.setLongitude(bfLong);
 
                     mMap.clear();
                     mMap.addMarker(new MarkerOptions()
-                            .position(locationToLatLng(game.getRedFlagLocation()))
-                            .title("Red Flag")
+                            .position(locationToLatLng(game.getRedFlagLocation())).title("Red Flag")
                             .draggable(false));
-                    mMap.addCircle(new CircleOptions()
-                            .center(locationToLatLng(game.getRedFlagLocation()))
-                            .radius(10)
-                            .strokeColor(Color.RED)
-                            .fillColor(Color.RED));
                     mMap.addMarker(new MarkerOptions()
-                            .position(locationToLatLng(game.getBlueFlagLocation()))
-                            .title("Blue Flag")
+                            .position(locationToLatLng(game.getBlueFlagLocation())).title("Blue Flag")
                             .draggable(false));
-                    mMap.addCircle(new CircleOptions()
-                            .center(locationToLatLng(game.getBlueFlagLocation()))
-                            .radius(10)
-                            .strokeColor(Color.BLUE)
-                            .fillColor(Color.BLUE));
+
+                    if (!holding) {
+                        mMap.addCircle(new CircleOptions()
+                                .center(locationToLatLng(game.getRedFlagLocation())).radius(RADIUS)
+                                .strokeColor(Color.RED).fillColor(Color.RED));
+                        mMap.addCircle(new CircleOptions()
+                                .center(locationToLatLng(game.getBlueFlagLocation())).radius(RADIUS)
+                                .strokeColor(Color.BLUE).fillColor(Color.BLUE));
+                    }
 
                     if (liveLocationsMap != null) {
                         for (String userName : liveLocationsMap.keySet()) {
-                            String teamColor = (String) liveLocationsMap.get(userName).get("teamColor");
-                            if (teamColor != null) {
+                            String playerTeamColor = (String) liveLocationsMap.get(userName).get("teamColor");
+                            if (playerTeamColor != null) {
                                 HashMap<String, Double> locationsMap = (HashMap<String, Double>) liveLocationsMap.get(userName).get("locations");
                                 LatLng playerLocation = new LatLng(locationsMap.get("latitude"), locationsMap.get("longitude"));
-                                mMap.addMarker(new MarkerOptions().position(playerLocation).title(teamColor + " " + userName));
+                                mMap.addMarker(new MarkerOptions().position(playerLocation).title(playerTeamColor + " " + userName));
+                                if (playerTeamColor.equals("red")) {
+                                    Location.distanceBetween(locationsMap.get("latitude"),
+                                            locationsMap.get("longitude"), game.blueFlag.getLatitude(), game.blueFlag.getLongitude(), distance);
+                                    Log.d("Debug", "Distance: " + Float.toString(distance[0]));
+                                    if (distance[0] <= RADIUS)
+                                        mCaptureButton.setVisibility(View.VISIBLE);
+                                } else if (playerTeamColor.equals("blue")) {
+                                    Log.d("Debug", "Distance: " + Float.toString(distance[0]));
+                                    Location.distanceBetween(locationsMap.get("latitude"),
+                                            locationsMap.get("longitude"), game.redFlag.getLatitude(), game.redFlag.getLongitude(), distance);
+                                    if (distance[0] <= RADIUS)
+                                        mCaptureButton.setVisibility(View.VISIBLE);
+                                }
                             }
                         }
                     }
@@ -207,17 +292,5 @@ public class GameActivity
     private LatLng locationToLatLng(Location loc) {
         return (loc == null) ? new LatLng(38.985933, -76.942792)
                 : new LatLng(loc.getLatitude(), loc.getLongitude());
-    }
-
-    private double distanceInMeters(double lat1, double lng1, double lat2, double lng2) {
-        int r = 6371; // average radius of the earth in km
-        double dLat = Math.toRadians(lat2 - lat1);
-        double dLon = Math.toRadians(lng2 - lng1);
-        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-                        * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double d = r * c;
-        return d/1000;
     }
 }
