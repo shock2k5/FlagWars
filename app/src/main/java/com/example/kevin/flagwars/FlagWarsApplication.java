9565e25afac9c3c53c1c26d43096909fa9a19807
Aditya Ayyakad <njaditya@gmail.com>
2016-05-17 21:50:40 -0400

    Final Commit.

--- app/src/main/java/com/example/kevin/flagwars/FlagWarsApplication.java
+++ app/src/main/java/com/example/kevin/flagwars/FlagWarsApplication.java
@@ -2,6 +2,18 @@ package com.example.kevin.flagwars;
 
 import android.app.Application;
 
+/**
+ *
+ * Created by:
+ * Aditya Ayyakad
+ * Brian Lemons
+ * Ezra Schwartz
+ * Kevin Coleman
+ * Michael Kalu
+ *
+ * CMSC436, Spring 2016 University of Maryland, College Park
+ *
+ */
 public class FlagWarsApplication extends Application {
 
     @Override
